4346db77d916b17e93d352e993931e184e27b17c
dell <dell@DESKTOP-VRQMF57>
2023-03-28 12:03:07 +0530

    Arraycodes

---
+++ src/main/java/arrays/arraycode3.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package arrays;
+
+/**
+ * @author dell
+ *
+ */
+import java.util.Scanner;
+public class arraycode3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the count of classes:");
+		Scanner sc=new Scanner(System.in);
+		int cls=sc.nextInt();
+		
+		int arr[][]=new int[cls][];
+		for (int i=0;i<arr.length;i++) {
+			System.out.println("Enter the student count inside the class"+(i+1));
+			
+				arr[i]=new int [sc.nextInt()];
+			}
+		
+		
+		for (int i=0;i<arr.length;i++) {
+			System.out.println("inside the class"+(i+1));
+			for (int j=0;j<arr[i].length;j++) {
+				System.out.println("Enter the marks of students"+(j+1));
+				arr[i][j]=sc.nextInt();
+
+			}
+		}
+		System.out.println("____");
+		for (int i=0;i<arr.length;i++) {
+			System.out.println("inside the class"+(i+1));
+			for (int j=0;j<arr[i].length;j++) {
+				System.out.println("The marks of student"+ (j+1+"is:"+(arr[i][j])));
+			
+		
+	}
+}
+
+	}
+
+}
